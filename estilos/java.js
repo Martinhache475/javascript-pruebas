802a7d0bfbc36ba2be761ef0fef72ac4660ccfa9
Martinhache475 <108502607+Martinhache475@users.noreply.github.com>
2022-07-16 07:50:10 -0300

    nuevo

---
+++ estilos/java.js
@@ -0,0 +1,77 @@
+let name = "programación" + " no es tan facil.";
+let hello = " La" +  " Fucking ";
+let fin = " Dedicate a otra cosa. ";
+let saludo = "Le doy la bienvenida a ";
+let saludo2 = "el infierno."
+let todalafrase = saludo + saludo2 + hello + name + fin;
+console.log(todalafrase);
+document.write(todalafrase);
+
+let frase = "  " +  "Javascript " + "es un lenguaje" + " esencial para crear" + " Sitios de Web poderosos.";
+let completa = "El diablo" + " primero creo el infierno" + " y despues" + " creo los lenguajes de programacion. ";
+document.write(frase + completa); console.log(frase + completa);
+
+let space = "_________   ____________________";
+let spacex = space;
+document.write(spacex);
+
+let numeroString1 = "Soy el 10. ";
+let numeroString2 = "Soy 2 veces 10" + " = 20. ";
+let numeroString3 = "Soy 3 veces 10" + " = 30. ";
+let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
+console.log(concatenaNumeros);
+document.write(concatenaNumeros);
+
+let sexo ; sexo = prompt("introduce 1 si eres Hombre, 2 si eres Mujer o 0 si eres otro"); if (sexo == 1 ) {alert ("hombre");} else if (sexo ==2) {alert("mujer");} else {alert("Otro");}
+
+let edad;
+edad = prompt("Si eres hombre Introduce tu edad");
+if (edad >= 18){
+ alert("Puedes entrar, eres mayor de edad.");
+} else {
+ alert("No puedes entrar, eres menor de edad.");
+}
+
+let edadm;
+edadm = prompt("si eres mujer Introduce tu edad");
+if (edadm >= 20){
+ alert("Puedes entrar, eres mayor de edad.");
+} else {
+ alert("No puedes entrar, eres menor de edad.");
+}
+
+let edadind;
+edadind= prompt("si eres otro Introduce tu edad");
+if (edadind >= 19){
+ alert("Puedes entrar, eres mayor de edad.");
+} else {
+ alert("No puedes entrar, eres menor de edad.");
+}
+
+
+let diaSemana = prompt("¿Qué día es hoy?");
+switch (diaSemana) {
+case "lunes":
+alert("Hoy es lunes. Te toca rutina aerobica + cardio");
+ break;
+case "martes":
+alert("Hoy es martes. Animo, hoy toca pecho y espalda");
+ break;
+case "miércoles":
+alert("Hoy es miércoles. Estas listo? hoy toca aerobico + piernas"); //podria poner un boolean
+ break;
+case "jueves":
+alert("Hoy es jueves. Ponte la malla y vamos a nadar un rato");
+ break;
+ case "viernes":
+alert("Hoy es viernes. Y tu cuerpo lo sabe: rutina completa: nado, correr y bici");
+ break;
+case "sábado":
+alert("Hoy es sábado. Hoy tranqui, solo cardio");
+ break;
+case "domingo":
+alert("Hoy es domingo. Rutina desintoxicante: caminata + escaladora");
+ break;
+ default:
+alert("Escribe el día de la semana en minúsculas.");
+}
