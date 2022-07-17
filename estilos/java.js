992915c00eb95e78b0685c2b26f463ccf05fa310
Martinhache475 <108502607+Martinhache475@users.noreply.github.com>
2022-07-17 09:25:55 -0300

    hola

--- estilos/java.js
+++ estilos/java.js
@@ -5,11 +5,16 @@ let saludo = "Le doy la bienvenida a ";
 let saludo2 = "el infierno."
 let todalafrase = saludo + saludo2 + hello + name + fin;
 console.log(todalafrase);
-document.write(todalafrase);
 
+function saludar (){
+    alert (todalafrase)
+}
+ 
 let frase = "  " +  "Javascript " + "es un lenguaje" + " esencial para crear" + " Sitios de Web poderosos.";
-let completa = "El diablo" + " primero creo el infierno" + " y despues" + " creo los lenguajes de programacion. ";
-document.write(frase + completa); console.log(frase + completa);
+let completa = "El diablo" + " primero creo el infierno" + " y despues" + " creo los lenguajes de programación";
+function lafrase(){
+    alert(frase + completa)
+}
 
 let space = "_________   ____________________";
 let spacex = space;
@@ -20,32 +25,72 @@ let numeroString2 = "Soy 2 veces 10" + " = 20. ";
 let numeroString3 = "Soy 3 veces 10" + " = 30. ";
 let concatenaNumeros = numeroString1 + numeroString2 + numeroString3;
 console.log(concatenaNumeros);
-document.write(concatenaNumeros);
-
-let sexo ; sexo = prompt("introduce 1 si eres Hombre, 2 si eres Mujer o 0 si eres otro"); if (sexo == 1 ) {alert ("hombre");} else if (sexo ==2) {alert("mujer");} else {alert("Otro");}
-
-let edad;
-edad = prompt("Si eres hombre Introduce tu edad");
-if (edad >= 18){
- alert("Puedes entrar, eres mayor de edad.");
-} else {
- alert("No puedes entrar, eres menor de edad.");
+function concatenar (){
+    alert(concatenaNumeros)
 }
 
-let edadm;
-edadm = prompt("si eres mujer Introduce tu edad");
-if (edadm >= 20){
- alert("Puedes entrar, eres mayor de edad.");
-} else {
- alert("No puedes entrar, eres menor de edad.");
+function masc(){let edad;
+    edad = prompt("Introduce tu edad");
+    if (edad >= 18){
+    alert("Puedes entrar, eres mayor de edad.");
+    } else {
+    alert("No puedes entrar, eres menor de edad.");
+    }
 }
 
-let edadind;
-edadind= prompt("si eres otro Introduce tu edad");
-if (edadind >= 19){
- alert("Puedes entrar, eres mayor de edad.");
-} else {
- alert("No puedes entrar, eres menor de edad.");
+function fem(){let edad;
+    edad = prompt("Introduce tu edad");
+    if (edad >= 20){
+    alert("Puedes entrar, eres mayor de edad.");
+    } else {
+    alert("No puedes entrar, eres menor de edad.");
+    }
+}
+
+//let sexo ; sexo = prompt("introduce 1 si eres Hombre, 2 si eres Mujer o 0 si eres otro"); if (sexo == 1 ) {alert ("hombre");} else if (sexo ==2) {alert("mujer");} else {alert("Otro");}
+
+//let num; alert(prompt("indique la cantidad de vueltas")); console.log(num); for (num;   document.write(i `hola mundo<br>`);
+  //  }
+
+
+
+//let edadm;
+//edadm = prompt("si eres mujer Introduce tu edad");
+//if (edadm >= 20){
+ //alert("Puedes entrar, eres mayor de edad.");
+//} else {
+ //alert("No puedes entrar, eres menor de edad.");
+//}
+
+//let edadind;
+//edadind= prompt("si eres otro Introduce tu edad");
+//if (edadind >= 19){
+ //alert("Puedes entrar, eres mayor de edad.");
+//} else {
+ //alert("No puedes entrar, eres menor de edad.");
+//}
+
+function monday(){
+    alert("Hoy es lunes. Te toca rutina aerobica + cardio")
+}
+function tuesday(){
+    alert("Hoy es martes. Animo, hoy toca pecho y espalda");
+}
+function wensday(){
+    alert("Hoy es miércoles. Estas listo? hoy toca aerobico + piernas");
+}
+
+function thursday(){
+   alert("Hoy es jueves. Ponte la malla y vamos a nadar un rato");
+}
+function friday(){
+    alert("Hoy es viernes. Y tu cuerpo lo sabe: rutina completa: nado, correr y bici");
+}
+function saturday(){
+    alert("Hoy es sábado. Hoy tranqui, solo cardio");
+}
+function sunday(){
+    alert("Hoy es domingo. Rutina desintoxicante: caminata + escaladora");
 }
 
 
@@ -59,19 +104,19 @@ alert("Hoy es martes. Animo, hoy toca pecho y espalda");
  break;
 case "miércoles":
 alert("Hoy es miércoles. Estas listo? hoy toca aerobico + piernas"); //podria poner un boolean
- break;
+break;
 case "jueves":
 alert("Hoy es jueves. Ponte la malla y vamos a nadar un rato");
  break;
  case "viernes":
 alert("Hoy es viernes. Y tu cuerpo lo sabe: rutina completa: nado, correr y bici");
- break;
+break;
 case "sábado":
 alert("Hoy es sábado. Hoy tranqui, solo cardio");
  break;
 case "domingo":
 alert("Hoy es domingo. Rutina desintoxicante: caminata + escaladora");
- break;
- default:
+break;
+default:
 alert("Escribe el día de la semana en minúsculas.");
 }
